24b1295decedcc3fbaa874ff300c161b034c8b35
Galeo <Arichikari@yandex.ru>
2022-09-04 14:53:42 +0300

    SeminarC#4/

---
+++ SeminarC#4/1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Ввод данных
+Console.WriteLine("введите число A");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число B");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int deal = num1;
+// Условия работы программы
+for (int num3 = 1; num3 < num2; num3++){
+deal = deal * num1;
+}
+Console.WriteLine("A в степени B равно: " + deal);
+
+// Вывод ответа
---
+++ SeminarC#4/3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из произвольного элементов и выводит их на экран.
+// Ввод данных
+int [] numbers = new int[8];
+Console.Write("[");
+
+// Условие работы программы
+for (int num = 0; num < numbers.Length; num++)
+ {
+    numbers [num] = new Random().Next(0, 11);
+    Console.Write(" " + Method (num) + " ");
+ }
+Console.Write("]");
+
+int Method (int num)
+{
+    return numbers[num];
+}
+
+// Вывод ответа
