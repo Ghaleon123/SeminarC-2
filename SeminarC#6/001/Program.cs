3a057db0481ecdb8aec9abba7d98498c054903a9
Galeo <Arichikari@yandex.ru>
2022-09-11 14:55:42 +0300

    SeminarC#6/

---
+++ SeminarC#6/001/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+// Ввод данных
+
+Console.WriteLine("Cколько чисел больше 0 ввёл пользователь ");
+Console.Write("Введи число numМ(количество чисел): ");
+int numM = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[numM];
+
+// Условия работы программы
+void InputNumbers(int numM){
+for (int num1 = 0; num1 < numM; num1++)
+  {
+    Console.Write($"Введи {num1+1} число: ");
+    massiveNumbers[num1] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int num1 = 0; num1 < massiveNumbers.Length; num1++)
+  {
+    if(massiveNumbers[num1] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(numM);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+
+// Вывод ответа
---
+++ SeminarC#6/002/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+// Ввод данных
+Console.WriteLine($"\n Найти точку пересечения двух прямых \n");
+
+double[,] num3 = new double[2, 2];
+double[] cPnum = new double[2];
+
+// Условия выполнения программы
+void InputCoefficients(){
+  for (int num = 0; num < num3.GetLength(0); num++)
+  {
+    Console.Write($"Введите коэффициенты {num+1}-го уравнения (y = k * x + b):\n");
+    for (int num1 = 0; num1 < num3.GetLength(1); num1++)
+    {
+      if(num1==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      num3[num,num1] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] num3)
+{
+  cPnum[0] = (num3[1,1] - num3[0,1]) / (num3[0,0] - num3[1,0]);
+  cPnum[1] = cPnum[0] * num3[0,0] + num3[0,1];
+  return cPnum;
+}
+
+void OutputResponse(double[,] num3)
+{
+  if (num3[0,0] == num3[1,0] && num3[0,1] == num3[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (num3[0,0] == num3[1,0] && num3[0,1] != num3[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Decision(num3);
+    Console.Write($"\nТочка пересечения прямых: ({cPnum[0]}, {cPnum[1]})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(num3);
+
+// Вывод ответа
